83eb1addb6150a7af390c8cbacadd859a2ca52f8
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-05-07 21:23:59 +0200

    Solved some issue

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,43 @@
+import { useForm } from "react-hook-form";
 const Form = () => {
+  const { register, handleSubmit } = useForm();
+const onSubmit = data => console.log(data)
+  // const nameRef = useRef<HTMLInputElement>(null);
+  // const ageRef = useRef<HTMLInputElement>(null);
+  // const person = { name: "", age: 0 };
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   if (nameRef.current !== null) person.name = nameRef.current.value;
+  //   if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+  //   console.log(person);
+  // };
   return (
-    <form>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3 flex flex-col">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="control border-2" />
+        <input
+          // ref={nameRef}
+          {...register("name")}
+          id="name"
+          type="text"
+          className="control border-2"
+        />
       </div>
+      <div className="flex flex-col">
+        <label htmlFor="age">Age</label>
+        <input
+          // ref={ageRef}
+          {...register("age")}
+          id="age"
+          type="number"
+          className="control border-2"
+        />
+      </div>
+      <button className="button bg-green-600 h-10 w-40 rounded-lg text-white">
+        Submit
+      </button>
     </form>
   );
 };
